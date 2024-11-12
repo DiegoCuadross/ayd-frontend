06e44eb511455a21696aef5b65ac4fe535e6ab97
DiegoCuadross <dcuadrso@gmail.com>
2024-11-12 13:56:05 -0500

    avance

--- src/autenticacion/Login.jsx
+++ src/autenticacion/Login.jsx
@@ -8,17 +8,37 @@ export default function Login() {
           <h2>¿No tienes cuenta?</h2>
           <h2 className="text-red-500 underline">Registrarse</h2>
         </div>
-
-        <div className="flex items-center justify-center">
-        <h1>Iniciar Sesión</h1>
+        <div className="flex align-center justify-center flex-col h-[600px]">
+        <div className="flex items-center justify-around flex-col h-[300px]">
+          <h1 className="text-2xl font-semibold">Iniciar Sesión</h1>
+          <div className="flex flex-col justify-between h-[130px]">
+            <div className="flex flex-col bg-[#f3f4f6] py-1 px-2 w-[400px] rounded-md">
+              <label htmlFor="email">Email</label>
+              <input
+                id="email"
+                name="name"
+                type="email"
+                required
+                placeholder="example.email@gmail.com"
+                className="bg-transparent "
+              />
+            </div>
+            <div className="flex flex-col bg-[#f3f4f6] py-1 px-2 w-[400px] rounded-md">
+              <label htmlFor="email">Password</label>
+              <input
+                id="password"
+                name="name"
+                type="password"
+                required
+                placeholder="Enter at least 8+ characters"
+                className="bg-transparent"
+              />
+            </div>
+          </div>
         </div>
-
-        <div className="">
-        <label htmlFor="first-name" className="block text-sm/6 font-semibold text-gray-900">
-              First name
-            </label>
         </div>
-
+        
+        
       </div>
       <div div className=" w-1/2 bg-[#aa1916] ">
         <div></div>
